ed22f675aef3234fad1b1dfaf3639663d4980d63
isack <isackbreton@live.ca>
2019-11-06 19:35:28 -0500

    OK

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,10 @@
 #define Rouge 3
 #define Jaune 4
 
+#define ligne 1
+#define Millieux 2
+#define Vide 0
+
 /*
   Couleur où se trouve le ballon
  */
@@ -17,14 +21,14 @@
 
 //declaration fonction
 float PID(float TargetSpeed);
-void Avancer(float Distance,int TempsAttente);
-void Tourner(int Direction, int Rotation,int TempsAttente);
-void Rotation180(int NbRot,int TempsAttente);
+void Avancer(float Distance,int TempsAttente,float TargetSpeed,int arret);
+void Tourner(int Direction, int Rotation,int TempsAttente,int arret,float TargetSpeed);
+void Rotation(int angle,int TempsAttente);
 void RadiusTurn(float Radius,int angle,int Direction,int TempsAttente);
 int detectionCouleur();
 void servomoteurPrendre ();
 void servomoteurLacher ();
-int detecteurLigne(float TargetSpeed);
+int detecteurLigne(float TargetSpeed,int type);
 void TrouverBallon();
 
 //Initialisation des variables globales
@@ -57,14 +61,118 @@ void setup() {
  */
 void loop() 
 {
-  if(ROBUS_IsBumper(3)==true)
+/*
+======================
+Code Robot A
+======================
+ */
+if(ROBUS_IsBumper(3)==true)
+{
+  delay(1000);
+  //180 pour aller vers la ligne
+  //Rotation(180,500);
+  // reculer pour trouver la ligne
+  do
   {
-    while (detecteurLigne(0)==0)
+    Avancer(1,0,0.1,0);
+  }while (detecteurLigne(speed,0)== Vide);
+  MOTOR_SetSpeed(0,0);
+  MOTOR_SetSpeed(1,0);
+
+  // Tourner pour etre parallèle a la ligne
+  Tourner(0,90,1000,1,speed);
+
+  //Vérifier la detection de la ligne
+  if (detecteurLigne(speed,0)!=1)
+  {
+      delay(5000);
+  }
+
+  do
+  {
+    detecteurLigne(speed,1);
+  }while (detectionCouleur()==0);
+
+  while (detectionCouleur()!=Couleur)
+  {
+    Avancer(15.23,500,speed,1);
+    Tourner(0,90,500,speed,1);
+    Avancer(50, 500,speed,1);//28.73+ distance roue capteur
+    do
     {
-      Avancer(-1,0);
+      detecteurLigne(speed,1);
+    }while (detectionCouleur()==0);
+  }
+}
+
+
+/*
+======================
+Code de test fonction
+======================
+ */
+/*  if(ROBUS_IsBumper(3)==true)
+  {
+    Serial.println("bumper arriere");
+       SERVO_Enable(0);
+        SERVO_SetAngle(0, 20);
+        delay(500);
+        SERVO_SetAngle(0, 150);
+        delay(2000);
+        SERVO_SetAngle(0, 20);
+        delay(500);
+        SERVO_Disable(0);
+        int i=0;
+    while (i<=65000)
+    {
+      detecteurLigne(0.3,150,150,7);
+      i++;
     }
-    Avancer(-28,0);
-    delay(1000);
+    Serial.println("fin");
+    delay(500);
+    
+  }
+   
+
+  if(detectionCouleur() == Couleur)
+  {
+    Avancer(5,1000);
+    servomoteurPrendre();
+    Rotation(180,100);
+    //Tourner(0,180,0);
+    Avancer(112.08,0);
+    servomoteurLacher();
+    Tourner(0,135,0);
+    Avancer(60,0);
+  }
+
+  while (detectionCouleur() != Couleur)
+  {
+    Rotation(135,100);
+     //Tourner(0,135,0);
+     Avancer(151.4,0);
+  }
+  
+  if(detectionCouleur() == Couleur)
+  {
+    Avancer(5,1000);
+    servomoteurPrendre();
+    Tourner(0,180,0);
+    Avancer(112.8,0);
+    servomoteurLacher();
+    Tourner(0,135,0);
+    Avancer(60,0);
+  }
+  }
+
+  
+
+    while (detectionCouleur() != Couleur)
+    {
+      detecteurLigne(0);
+    }
+    //Avancer(-28,0);
+   // delay(1000);
 
     while (detecteurLigne(0)==0)
     {
@@ -97,49 +205,66 @@ void loop()
 
     //aller position pas dérengente
   }
+*/
 }
-/*
-==========================
+
+/*==========================
 Avancer
 ==========================
 */
 
- void Avancer(float Distance,int TempsAttente)
+ void Avancer(float Distance,int TempsAttente,float TargetSpeed,int arret)
  {
     ENCODER_ReadReset(0);
     ENCODER_ReadReset(1); 
     unsigned int NbPulse=(3200*Distance)/(23.9389);
-    MOTOR_SetSpeed(0, speed);
-    MOTOR_SetSpeed(1, speed);
+    MOTOR_SetSpeed(0, TargetSpeed);
+    MOTOR_SetSpeed(1, TargetSpeed);
     unsigned int PulseCount=ENCODER_Read(0);
+    long Previous_time = millis();
+    long CurrentTime = millis();
+    unsigned int TimeSample = 0;
 
-    while(PulseCount<=NbPulse)
+   while(PulseCount<=NbPulse)
     {
+      Serial.println(PulseCount);   
+      TimeSample = CurrentTime-Previous_time;
       PulseCount=ENCODER_Read(0);
+         if (TimeSample>=100)
+        {
+          MOTOR_SetSpeed(1,PID(TargetSpeed));
+          MOTOR_SetSpeed(0, TargetSpeed);
+          Previous_time=CurrentTime;
+        }
+        CurrentTime=millis();
     }
+    if (arret==1){
     MOTOR_SetSpeed(0, 0);
     MOTOR_SetSpeed(1, 0);
+    }
   
     delay(TempsAttente);
   
     ENCODER_ReadReset(0);
     ENCODER_ReadReset(1);
+    Serial.println("Fin avancer");
   }
 /*
 ==========================
 Tourner
 ==========================
 */
- void Tourner(int Direction, int Rotation,int TempsAttente)
+ void Tourner(int Direction, int Rotation,int TempsAttente,int arret,float TargetSpeed)
  {
   ENCODER_ReadReset(0);
   ENCODER_ReadReset(1);
 
   unsigned int NbPulse=((2*3200*3.14*L*Rotation*0.965)/(23.9389*360));//(2*PI*18.350(Distance entre roue)*3200*Rotation)/(360*23.9389(Nb cm par tour de roue))
-  float ActualSpeed = 0.1;
-  long Previous_time = millis();
-  long CurrentTime = millis();
-  unsigned int TimeSample = 0;
+  float ActualSpeed = 0.2;
+  if (TargetSpeed != 0.0)
+  {
+    ActualSpeed=TargetSpeed;
+  }
  if (Direction==0)
   {
     MOTOR_SetSpeed(1,0);
@@ -150,36 +275,31 @@ Tourner
   }
   MOTOR_SetSpeed(Direction, ActualSpeed);
   unsigned int PulseCount = ENCODER_Read(Direction);
+  Serial.println(NbPulse);
   while(PulseCount<=NbPulse)
   {    
-        
-        TimeSample = CurrentTime-Previous_time;
-        if (TimeSample>=300 && ActualSpeed<speed)
-        {
-          ActualSpeed=ActualSpeed*1.15;
-          MOTOR_SetSpeed(Direction,ActualSpeed);
-          Previous_time=CurrentTime;
-          TimeSample = 0;
-        }
-        CurrentTime=millis();
-
-      PulseCount=ENCODER_Read(Direction);
+    MOTOR_SetSpeed(Direction,ActualSpeed);
+    PulseCount=ENCODER_Read(Direction);
+    Serial.println(PulseCount);
   }
-  MOTOR_SetSpeed(Direction, 0);
+  if (arret==1){
+  MOTOR_SetSpeed(Direction, 0);}
+
   ENCODER_ReadReset(0);
   ENCODER_ReadReset(1);
   delay(TempsAttente);
+  Serial.println("Fin de Tourner");
  }
  /*
 ==========================
 Rotation 180
 ==========================
 */
-void Rotation180 (int NbRot, int TempsAttente)
+void Rotation (int Angle, int TempsAttente)
 {
   ENCODER_ReadReset(0);
   ENCODER_ReadReset(1);
-  unsigned int NbPulse=((3200*3.14*L*NbRot * 0.972)/(2*23.9389));//.972=correction robot jean-claude
+  unsigned int NbPulse=((3200*3.14*L*Angle* 0.972)/(360*23.9389));//.972=correction robot jean-claude
   float ActualSpeed = 0.1;
   long Previous_time = millis();
   long CurrentTime = millis();
@@ -187,11 +307,12 @@ void Rotation180 (int NbRot, int TempsAttente)
   MOTOR_SetSpeed(0, ActualSpeed);
   MOTOR_SetSpeed(1,-ActualSpeed);
   unsigned int PulseCount=ENCODER_Read(0);
+  Serial.println(NbPulse);
   while(PulseCount<=NbPulse)
   {
        
         TimeSample = CurrentTime-Previous_time;
-        if (TimeSample>=200 && ActualSpeed<speed && PulseCount<=2500)
+        if (TimeSample>=200 && ActualSpeed<speed && PulseCount<NbPulse/2)
         {
           ActualSpeed=ActualSpeed*1.2;
           MOTOR_SetSpeed(0,ActualSpeed);
@@ -199,13 +320,14 @@ void Rotation180 (int NbRot, int TempsAttente)
           Previous_time=CurrentTime;
           TimeSample = 0;
         }
-        if (PulseCount>=(NbPulse-2500) && TimeSample>=200)// 10cm = 1330 pulse
+        if (PulseCount>=(NbPulse/2) && TimeSample>=200 && ActualSpeed>0.15)// 10cm = 1330 pulse
       {
-        ActualSpeed=ActualSpeed*0.95;
+        ActualSpeed=ActualSpeed*0.75;
         MOTOR_SetSpeed(0,ActualSpeed);
         MOTOR_SetSpeed(1,-ActualSpeed);
         Previous_time=CurrentTime;
         TimeSample = 0;
+        Serial.println(ActualSpeed);
 
       }
         CurrentTime=millis();
@@ -299,8 +421,9 @@ Servomoteurs prendre
 
 void servomoteurPrendre ()
 {
+   SERVO_Enable(0);
+  SERVO_SetAngle(0, 20);
   SERVO_Enable(0);
-  SERVO_SetAngle(0, 180);
 }
 
 /*
@@ -311,7 +434,8 @@ Servomoteurs lacher
 
 void servomoteurLacher ()
 {
-  SERVO_SetAngle(0, 0);
+  SERVO_Enable(0);
+  SERVO_SetAngle(0, 150);
   SERVO_Disable(0);
 }
 
@@ -320,59 +444,65 @@ void servomoteurLacher ()
 Dectecteur de ligne
 ==========================
 */
- int detecteurLigne(float TargetSpeed)
+ int detecteurLigne(float TargetSpeed,int type)
  {
-  int vitesseRoue=0;
-  digitalWrite(35,HIGH);
+  int Detect=0;
   float tension = analogRead(A8)*5.0/1023.0;
   Serial.println(tension);
-  delay(500);
 
-  Serial.println(analogRead(60));
+ 
+
+  //Serial.println(analogRead(60));
   //En fonction de détection de Blanc 
   if (tension<0.5) //option 1(Aucun) 0.00 
   {
-    vitesseRoue=0;
+    Detect=Millieux;
   }
-  else if(tension< 1) //option 2(X3) 0.71
+  else if(tension < 1) //option 2(X3) 0.71
   {
-
+    Detect=ligne;
   }
   else if(tension< 1.9) //option 3(X2) 1.51
   {
-
+    Detect=ligne;
   }
   else if(tension< 2.40) //option 4(X2,X3) 2.14
   {
-
+    if (type==1)
+    {
+      MOTOR_SetSpeed(1,0);
+      MOTOR_SetSpeed(0,0);
+      Tourner(1,5,0,0,-TargetSpeed*1.7);
+    }
+    Detect=ligne;
   }
   else if(tension< 3) //option 5(X1) 2.85
   {
-
+    Detect=ligne;
   }
   else if(tension< 3.9) //option 6(X1,X3) 3.57
   {
-
+   
+   Detect=ligne;
   }
   else if(tension< 4.5) //option 7(X1,X2) 4.28
   {
-
+    if (type==1)
+    {
+      MOTOR_SetSpeed(1,0);
+      MOTOR_SetSpeed(0,0);
+      Tourner(0,5,0,0,-TargetSpeed*1.7);
+    }
+    Detect=ligne;
   }
   else //option 8(X1,X2,X3) 5
   {
-
+    Detect=Vide;   
   }
+    Avancer(0.25,0,-TargetSpeed,0);
 
-
-  /*int encodeur_0 = ENCODER_Read(0);
-  int encodeur_1 = ENCODER_Read(1);
-  Erreur = encodeur_0 - encodeur_1;
-  //Serial.println(Erreur);
-  pid = (Erreur * Kp) + ((Erreur*Ki)/sec) + TargetSpeed;
-  //Serial.println(pid); 
-  return pid;*/
-  delay(1000);
-  return vitesseRoue;
+  
+  return Detect;
  }
  
  /*
@@ -390,9 +520,9 @@ Dectecteur de ligne
                        // wait for this much time before printing next value
 
     //Les valeurs pour la longueure des rayons sont inconnue pour l'instant. Nous allons utiliser le therme: Longueur_des_rayons_IR
-  int Longeur_des_rayons_IR ;
+  int Longeur_des_rayons_IR =0;
   int ballon_trouve ;
-  if (ballon_trouve = Longeur_des_rayons_IR)
+  if (ballon_trouve == Longeur_des_rayons_IR)
     {
       /*
       ==========================
