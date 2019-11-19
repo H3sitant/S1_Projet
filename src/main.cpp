3893db56613ccc6a83afc4d782e3d6f961a5f5fb
isack <isackbreton@live.ca>
2019-11-19 15:23:58 -0500

    Midi dans le code

--- src/main.cpp
+++ src/main.cpp
@@ -33,22 +33,47 @@ int detecteurLigne(float TargetSpeed,int type);
 void TrouverBallon();
 uint16_t Distance (uint8_t capteurId);
 void Rotation1 (int Angle, int TempsAttente,int cote);
-int comparaison_midi(int instrument, int temps);
+int comparaison_midi(int instrument, int temps, int partition_C[2][70]);
+void choix_partition (int partition_C[2][70], int partition_I[2][70]);
 
-//traitement midi
-int partition_1[16];
-int partition_2[16];
-int partition_3[16];
+/*
+====================
+traitement midi
+====================
+*/
+//partition
+int partition_F[2][70] =  {//facile
+                          { 0,0,0,0 ,0,0,0,0 ,0,0,0,0 ,0,0,0,0,  1,0,1,0, 1,0,1,0, 1,0,1,0 ,1,0,1,0,  0,0,0,0, 1,0,0,0 ,0,0,0,0, 1,0,0,0,  0,0,0,0 ,1,0,0,0 ,0,0,0,0, 1,0,0,0,  1,0,0,0, 0,0},
+                          { 1,0,1,0 ,1,0,1,0 ,1,0,1,0 ,1,0,1,0,  0,0,0,0 ,0,0,0,0 ,0,0,0,0 ,0,0,0,0,  1,0,1,0 ,1,0,1,0 ,1,0,1,0 ,1,0,1,0,  1,0,1,0 ,1,0,1,0 ,1,0,1,0 ,1,0,1,0,  1,0,0,0, 0,0}
+                          };
+int partition_M[2][70] =  {//moyenne
+                          { 1,0,0,0 ,0,0,0,0 ,1,0,0,0 ,1,0,0,0,  0,0,0,0 ,1,0,0,0 ,0,0,0,0 ,0,0,0,0,  1,0,0,0 ,1,0,1,0 ,0,0,1,0 ,0,0,0,0,  1,0,1,1 ,0,1,0,0 ,1,0,1,1 ,0,1,0,0,  1,0,0,0, 0,0},
+                          { 0,0,0,0 ,1,0,0,0 ,0,0,0,0 ,0,0,0,0,  1,0,0,0 ,0,0,0,0 ,1,0,0,0 ,1,0,0,0,  0,0,1,0 ,0,0,0,0 ,1,0,0,0 ,1,0,1,0,  0,1,0,0 ,1,0,1,1 ,0,1,0,0 ,1,0,1,1,  0,0,0,0, 0,0}
+                          };
+int partition_D[2][70] =  {//difficile
+                          { 0,0,0,0 ,1,0,0,0 ,0,0,0,0 ,1,0,0,0,  0,0,1,0 ,0,0,1,0 ,0,0,1,0 ,0,0,1,0,  0,0,1,0 ,0,0,1,0 ,0,1,0,0 ,1,1,1,1,  1,0,0,0 ,0,0,0,0 ,0,0,0,0 ,0,0,0,0, 0,0,0,0 ,0,0},
+                          { 1,0,1,0 ,0,0,1,0 ,0,0,1,0 ,0,0,1,0,  1,1,0,0 ,1,1,0,0 ,1,1,0,0 ,1,1,0,0,  1,1,0,1 ,1,0,0,0 ,1,0,1,1 ,0,0,0,0,  1,0,0,0 ,0,0,0,0 ,0,0,0,0 ,0,0,0,0, 0,0,0,0 ,0,0}
+                          };
+//temps
 unsigned int start_time;
-
+//instrument
 #define instrument1 40
 #define instrument2 30
-
+//dificulté
+#define facile 1
+#define moyen 2
+#define Difficile 3
+//résultat
 #define reussie 1
 #define mauvaise_note 2
 #define manquer 3
 #define Erreur 0
 
+/*
+=====================
+
+=====================
+*/
 //Initialisation des variables globales
 float speed=0.2;
 float L=18.350;
@@ -74,6 +99,12 @@ void setup()
  */
 void loop() 
 {
+
+  /*
+  choix de partition avec bouton
+  */
+  int partition_C[2][70];
+  //choix_partition ( *partition_C[2][70], partition_I[2][70]);
   //traitement_fichier(partition);
   if (millis()-start_time<=16.125)
   {
@@ -91,10 +122,10 @@ void loop()
         int retour;
         if (incomingByte==instrument1)
         {
-          retour=comparaison_midi(instrument1,millis()-start_time);
+          retour=comparaison_midi(instrument1,millis()-start_time, partition_C);
         }else
         {
-          retour=comparaison_midi(instrument2,millis()-start_time);
+          retour=comparaison_midi(instrument2,millis()-start_time, partition_C);
         }
         
         if (retour==Erreur)
@@ -120,10 +151,20 @@ void loop()
 Comparaison midi
 ====================================
  */
-int comparaison_midi (int instrument, int temps)
+int comparaison_midi (int instrument, int temps, int partition_C[2][70])
 {
+  //trouver l'instrument
+  if(instrument==instrument1)
+  {
+    instrument=0; //position dans tableau
+  }else
+  {
+    instrument=1;//position dans tableau
+  }
+
   //trouver le reste pour pouvoir identifier la position du coups dans le temps
   int positionT = temps % 250;
+
   if (positionT<=125)
   {
     positionT= (temps-positionT)/250; //temps du coups arondi a la baisse
@@ -132,22 +173,37 @@ int comparaison_midi (int instrument, int temps)
     positionT= (temps-positionT)/250+1;//temps du coups aroudi a la hausse
   }
   //vérifier si les dimention son bonne
-  if (positionT>=16)
+  if (positionT>=64)
   {
     return 0;
   }
   else// comparaison du coups avec le fichier midi
   {
-    if(partition_1[positionT]==0)// pas de coups a se moment
+    if(partition_C[positionT][0]==0 && partition_F[positionT][1]==0)// pas de coups a se moment
     {
       return manquer;
-    }else if(partition_1[positionT]==instrument)// il ya un coups a se moment et ces le bon instrument
+    }else if(partition_C[positionT][instrument]==1)// il ya un coups a se moment et ces le bon instrument
     {
       return reussie;
     }else //il a un coups a se moment mais ce n'est pas le bon instrument
     {
       return mauvaise_note;
     }
+  } 
+}
+
+/*
+=====================================
+Choix de partition
+=====================================
+*/
+void choix_partition (int partition_C[2][70], int partition_I[2][70])
+{
+  for (int i=0; i<2; i++)
+  {
+    for (int j=0; j<70; j++)
+    {
+      partition_C[i][j]=partition_I[i][j];//innitialisation de la partition choisie
+    }
   }
-  
 }
